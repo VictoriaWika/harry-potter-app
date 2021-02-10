2fd24228241a10393e6d0040c8a74ab9f5c5772c
Victoria Wika <victoriawika@icloud.com>
2021-02-10 14:07:27 +0100

    Add Card component and clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import AppHeader from './components/AppHeader'
 import getCharacters from './services/getCharacters'
+import Card from './components/Card'
+import createElement from './lib/createElement'
 
 export default function App() {
   const header = AppHeader('Hogwarts')
@@ -7,6 +9,20 @@ export default function App() {
 
   // fetch API
   getCharacters()
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
+    .then(characters => createCards(characters))
+    .catch(error => handleGetCharacterError(error))
+
+  function createCards(characters) {
+    const cards = characters.map(character => Card(character.name))
+    document.body.append(...cards)
+  }
+
+  function handleGetCharacterError(error) {
+    const errorMessage = createElement(
+      'strong',
+      { style: 'color: crimson;' },
+      error.message
+    )
+    document.body.append(errorMessage)
+  }
 }
